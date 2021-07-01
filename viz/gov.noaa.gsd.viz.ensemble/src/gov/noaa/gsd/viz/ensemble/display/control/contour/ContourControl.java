5f52c94aab8183e426659a52d6ca1b3d3f0ad78f
Timothy Jensen <timothy.jensen@noaa.gov>
2021-07-01 15:07:09 -0500

    VLab Issue #93753 - Fix values in ContourControls labels
    Change-Id: Ie0c86fa9aca0960958d818947d562675cae0cb6f

--- viz/gov.noaa.gsd.viz.ensemble/src/gov/noaa/gsd/viz/ensemble/display/control/contour/ContourControl.java
+++ viz/gov.noaa.gsd.viz.ensemble/src/gov/noaa/gsd/viz/ensemble/display/control/contour/ContourControl.java
@@ -16,14 +16,14 @@ import com.raytheon.uf.viz.core.rsc.DisplayType;
 import com.raytheon.uf.viz.core.rsc.capabilities.DensityCapability;
 
 /**
- * 
+ *
  * Supports interactive contour control display by changing the label increment
  * and a given value.
- * 
+ *
  * The implementation is to access the AbstractStylePreferences object of a grid
  * resource. A methods getStylePreferences() should be added in the
  * AbstractGridResource. The code is,
- * 
+ *
  * public AbstractStylePreferences getStylePreferences() { return
  * stylePreferences; }
  *
@@ -31,11 +31,13 @@ import com.raytheon.uf.viz.core.rsc.capabilities.DensityCapability;
  *
  * SOFTWARE HISTORY
  *
- * Date         Ticket#    Engineer    Description
- * ------------ ---------- ----------- --------------------------
- * Feb 28, 2017   19598         jing     Initial creation
- * Jun 27, 2017   19325         jing     Upgrade to 17.3.1
- * Jun 03, 2019   64512         ksunil   changes to absorb new labelingPreferences
+ * Date          Ticket#  Engineer  Description
+ * ------------- -------- --------- ------------------------------------------
+ * Feb 28, 2017  19598    jing      Initial creation
+ * Jun 27, 2017  19325    jing      Upgrade to 17.3.1
+ * Jun 03, 2019  64512    ksunil    changes to absorb new labelingPreferences
+ * Jul 01, 2021  93753    tjensen   Fix error in ValueLabelPreferences change
+ *
  * </pre>
  *
  * @author jing
@@ -66,7 +68,7 @@ public class ContourControl {
     /*
      * The grid resource be controlled
      */
-    private AbstractGridResource<?> rsc;
+    private final AbstractGridResource<?> rsc;
 
     /*
      * A flag with value 1 or -1, is used to change the density just a little to
@@ -83,7 +85,7 @@ public class ContourControl {
     /**
      * Initializes and returns the LabelingPreferences object of a grid
      * resource.
-     * 
+     *
      * @param rsc
      *            A grid resource
      * @return The labelingPreferences object of the grid resource
@@ -115,7 +117,7 @@ public class ContourControl {
 
     /**
      * Get the default value if the configured contour values are existing.
-     * 
+     *
      * @return The contour value or Float.NaN
      */
     public float getDefaultValue() {
@@ -132,7 +134,7 @@ public class ContourControl {
     /**
      * Change contour with user specified increment and value. The contours will
      * be as default if the value is "NaN", that only use the increment.
-     * 
+     *
      * @param increment
      *            The contour increment
      * @param contourValue
@@ -156,9 +158,12 @@ public class ContourControl {
         incr.setValues(new float[] { increment });
         labelingPreferences.setIncrement(Arrays.asList(incr));
 
-        ValuesLabelingPreferences val = new ValuesLabelingPreferences();
-        incr.setValues(new float[] { contourValue });
-        labelingPreferences.setValues(Arrays.asList(val));
+        if (values != null) {
+            ValuesLabelingPreferences val = new ValuesLabelingPreferences();
+            val.setValues(values);
+            labelingPreferences.setValues(Arrays.asList(val));
+        }
+        contourPreferences.setContourLabeling(labelingPreferences);
 
         rsc.setStylePreferences(contourPreferences);
 
@@ -171,7 +176,7 @@ public class ContourControl {
     /**
      * Change contour with user specified increment and value for a group grid
      * resources start from second resource.
-     * 
+     *
      * @param rscList
      *            A group grid resources
      * @param increment
@@ -235,7 +240,7 @@ public class ContourControl {
      * Force redrawing the contour of a grid resource. TODO:trick to force
      * redrawing by change density a little. Replace this code when there is any
      * other way.
-     * 
+     *
      * @param gRsc
      *            The grid resource
      */
